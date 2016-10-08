19d1efc60a20c022de9b2f0b9a82aa4e778c6791
김성찬 <dlrj1234@naver.com>
2016-10-08 13:49:36 +0900

    SumOfNumer.java 실행정상

--- homework4/src/homework4/ExamForWhile.java
+++ homework4/src/homework4/ExamForWhile.java
@@ -5,35 +5,39 @@ public class ExamForWhile {
 	public static void main(String[] args) {
 		Scanner s = new Scanner(System.in);
 		
-		System.out.print("반복문 연습하기1");
-		System.out.print("***********");
-		System.out.print("1. 부터 입력한 수까지 더하기");
-		System.out.print("2. 최대값/최소값구하기");
-		System.out.print("3. 입력받은숫자의 합계와 평균구하기");
-		System.out.print("4. 원하는 구구단 출력하기");
-		System.out.print("5. 짝수단/홀수단 출력하기");
-		System.out.print("6. 종료하기");
+		System.out.println("반복문 연습하기1");
+		System.out.println("***********");
+		System.out.println("1. 부터 입력한 수까지 더하기");
+		System.out.println("2. 최대값/최소값구하기");
+		System.out.println("3. 입력받은숫자의 합계와 평균구하기");
+		System.out.println("4. 원하는 구구단 출력하기");
+		System.out.println("5. 짝수단/홀수단 출력하기");
+		System.out.println("6. 종료하기");
 		System.out.print("원하는 메뉴는 >> ");
 		int number = s.nextInt();
 		
 		if(number == 1){
-		 SumOfNumbers sum = new SumOfNumbers();
-		 
+		 SumOfNumbers Sum = new SumOfNumbers();
+		 SumOfNumbers.Sum();
 		}
 		else if(number == 2){
 		 MaxAndMin Max = new MaxAndMin();
+		
 		}
         else if(number == 3){
 		 SumAndAvg Avg = new SumAndAvg();	
+		 
 		}
         else if(number == 4){
 	     GuGuDan Dan = new GuGuDan();
+	     
         }
         else if(number == 5){
 	     GuGuDan2 Dan = new GuGuDan2();
+	     
         }
         else if(number == 6){
-	       exit(0);
+	        System.exit(0);
         }
 
 						
--- homework4/src/homework4/SumOfNumbers.java
+++ homework4/src/homework4/SumOfNumbers.java
@@ -1,5 +1,24 @@
 package homework4;
 import java.util.*;
 public class SumOfNumbers {
-   
+  public static void Sum(){
+	  Scanner s = new Scanner(System.in);
+	  
+	  System.out.print("###1부터 입력한 수까지 더하기");
+	  System.out.println("숫자를 입력하세요");
+	  int sum = 0;
+
+	  while(true){
+		  System.out.print(">>");
+		  String n = s.nextLine();
+		  if(n.equals("Q")){
+			break;
+		  }
+		  else{
+			 sum = sum + Integer.parseInt(n);  
+		  }
+			  
+	  }
+	  System.out.print("총합은" +sum+ "입니다.");
+  }
 }
