21dc4b8b817bb00ba4e4271892aaef050be257d4
김성찬 <dlrj1234@naver.com>
2016-10-08 17:05:08 +0900

    GuGuDan 실행

--- homework4/src/homework4/GuGuDan.java
+++ homework4/src/homework4/GuGuDan.java
@@ -1,7 +1,31 @@
 package homework4;
-
+import java.util.*;
 public class GuGuDan {
    public static void Dan(){
+	  
+	   Scanner s = new Scanner(System.in);
+	  
+	   System.out.println("###원하는 구구단 출려하기");
+	   System.out.print("출력하고 싶은 단(Q:종료)");
 	   
+	   while(true){
+		   System.out.print(">>");
+		   String dan = s.nextLine();
+		   
+		   if(Integer.parseInt(dan) >1 && Integer.parseInt(dan)<10){
+			   for(int i = 0; i<10 ; i++){
+				   if(i%2 == 0)
+					   System.out.print(+Integer.parseInt(dan)+"*"+i+ "="+Integer.parseInt(dan)*i+"\n");
+				   else
+					   System.out.print(+Integer.parseInt(dan)+"*"+i+ "="+Integer.parseInt(dan)*i+"\n");
+			   }
+		   }
+		   else if(dan.equals("Q")){
+			   break;
+		   }
+		   else{
+			   System.out.println("===> 잘못입력");
+		   }
+	   }
    }
 }
