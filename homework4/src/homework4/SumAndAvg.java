6c29d0f27c0589fafc4759f72f5c7f6184adc804
김성찬 <dlrj1234@naver.com>
2016-10-08 15:09:46 +0900

    SumAndAvg 실행완료

--- homework4/src/homework4/SumAndAvg.java
+++ homework4/src/homework4/SumAndAvg.java
@@ -1,7 +1,29 @@
 package homework4;
-
+import java.util.*;
 public class SumAndAvg {
      public static void Avg(){
+    	 Scanner s = new Scanner(System.in);
     	 
+      System.out.print("###입력받은숫자의 합계와 평균 구하기");
+   	  System.out.println("숫자를 입력하세요");
+   	  int sum = 0;
+      double avg = 0;
+      int count = 0;
+   	  while(true){
+   		  System.out.print(">>");
+   		  String n = s.nextLine();
+   		  if(n.equals("Q")){
+   			break;
+   		  }
+   		  else {
+   			 sum = sum + Integer.parseInt(n); 
+   			 count ++;
+   			 avg = sum/count;
+   		  }
+   		  
+   	  }	  
+   	  
+   	  System.out.print("총합은" +sum+ "입니다.");
+   	  System.out.print("평균은" +avg+ "입니다");
      }
 }
